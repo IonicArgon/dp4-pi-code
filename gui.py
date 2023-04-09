1f8c735af20e95f1c72a982081154423635f2ba2
Marco Tan <marco.tan.200405@gmail.com>
2023-04-09 14:22:35 -0400

    ediot ting

--- gui.py
+++ gui.py
@@ -70,11 +70,11 @@ class TimerPage(tk.Frame):
         self.reset_button.pack(side="left", padx=10)
 
         # button to skip to 1 minute
-        self.skip_button = tk.Button(self, text="Skip to 1 minute", command=(lambda: self.__skip_to_one_minute))
+        self.skip_button = tk.Button(self, text="Skip to 1 minute", command=self.__skip_to_one_minute)
         self.skip_button.pack(pady=10, padx=10)
 
         # button to skip to halfway
-        self.skip_button_2 = tk.Button(self, text="Skip to halfway", command=(lambda: self.__skip_to_halfway))
+        self.skip_button_2 = tk.Button(self, text="Skip to halfway", command=self.__skip_to_halfway)
         self.skip_button_2.pack(pady=10, padx=10)
 
         # thread for timer because we do not want the GUI to freeze
